c75a1cfe8479649e082478b6d591db431504a76d
jnielsenDD <james.nielsen@datadoghq.com>
2021-07-09 11:14:01 +1000

    commit 1

--- src/DD_RUM.js
+++ src/DD_RUM.js
@@ -3,11 +3,11 @@ import { datadogLogs } from "@datadog/browser-logs";
 
 export const DD_RUM = () => {
   return datadogRum.init({
-    applicationId: "e9001005-ab04-4076-8809-55bd40ffb75e",
-    clientToken: "pubc49f33b53299ef5ffe25947ae0201451",
-    site: "datadoghq.com",
-    service: "siobhan-ra-318",
-    env: "dev-day",
+    applicationId: 'bcbc0c95-f848-4037-b884-e03c487f93ca',
+    clientToken: 'pube4e225caaa611546d5cea42895aafe80',
+    site: 'datadoghq.com',
+    service:'rum_app1',
+    env:'dev',
     version: "1.0.0",
     sampleRate: 100,
     trackInteractions: true,
@@ -18,10 +18,10 @@ export const DD_RUM = () => {
 
 export const DD_LOGS = () => {
   return datadogLogs.init({
-    clientToken: "pubc49f33b53299ef5ffe25947ae0201451",
-    site: "datadoghq.com",
-    service: "siobhan-ra-318",
-    env: "dev-day",
+    clientToken: "pube4e225caaa611546d5cea42895aafe80",
+    site: 'datadoghq.com',
+    service:'rum_app1',
+    env:'dev',
     version: "1.0.0",
     sampleRate: 100,
     trackInteractions: true,
